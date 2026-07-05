67ca2114f0d6f51f3fda89646c5c25f3abd1774c
sebasdedoors <sebastian28_04@hotmail.com>
2026-07-04 20:06:20 -0600

    "Change the way to use items"

--- Item/Item.py
+++ Item/Item.py
@@ -4,5 +4,10 @@ class Item:
         self.description = description
         self.item_level = item_level
         
-    def use(self, player):
-        pass
+    def useItem(self, player):
+        for name in player.inventory:
+            if name == self.name:
+                player.inventory.remove(name)
+                return f"{self.name} has been used by {player.name}!"
+        
+    
